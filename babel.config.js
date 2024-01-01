0166c1279896da70224a0d04c00cae5b85f2be6c
John Gutierrez <djterez88@gmail.com>
2024-01-01 02:58:31 -0800

    updated babel 5

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     presets: ["react-app"],
     plugins: [
-        ["@babel/plugin-transform-private-property-in-object", { loose: true }],
+        ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
     ],
 };
